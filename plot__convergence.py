93b278d5353f764401da9f2792e9794144c13c92
Tseng, Po-Hsun <zengbs@gmail.com>
2019-06-11 09:23:10 +0800

    minor update

--- plot__convergence.py
+++ plot__convergence.py
@@ -46,12 +46,14 @@ ax[1][0].plot( N,    SOR[2]            , 'r-o', mec='k', lw=2, ms=6,            
 ax[1][0].plot( N,     GS[2]            , 'g-o', mec='k', lw=2, ms=6,                label='Gauss-Seidel' )
 ax[1][0].plot( N, Jacobi[2]            , 'b-o', mec='k', lw=2, ms=6,                label='Jacobi' )
 ax[1][0].plot( N, 0.1*(N/N[0])**4.0, 'k-',     lw=2, ms=5, dashes=LStyle_DashDot, label='$N^{4}$' )
+ax[1][0].plot( N, SOR[2][0]*(N/N[0])**2.0, 'k>',     lw=2, ms=5, dashes=LStyle_DashDot, label='$N^{2}$' )
 
 #iterations
 ax[0][1].plot( N,    SOR[3]            , 'r-o', mec='k', lw=2, ms=6,                label='SOR' )
 ax[0][1].plot( N,     GS[3]            , 'g-o', mec='k', lw=2, ms=6,                label='Gauss-Seidel' )
 ax[0][1].plot( N, Jacobi[3]            , 'b-o', mec='k', lw=2, ms=6,                label='Jacobi' )
-ax[0][1].plot( N, 500*(N/N[0])**2.0, 'k-',     lw=2, ms=5, dashes=LStyle_DashDot, label='$N^{2}$' )
+ax[0][1].plot( N, 1e4*(N/N[0])**2.0, 'k-',     lw=2, ms=5, dashes=LStyle_DashDot, label='$N^{2}$' )
+ax[0][1].plot( N, 300*(N/N[0]), 'kv',     lw=2, ms=5, dashes=LStyle_DashDot, label='$N^{1}$' )
 
 #performance
 ax[1][1].plot( N,    SOR[4]            , 'r-o', mec='k', lw=2, ms=6,                label='SOR' )
@@ -93,10 +95,10 @@ ax[1][1].tick_params( which='both', tick2On=True, direction='in' )
 ax[0][0].legend( loc='lower left', numpoints=1, labelspacing=0.1, handletextpad=0.4,
               borderpad=0.4, handlelength=2.7, fontsize='large' )
 
-ax[1][0].legend( loc='lower right', numpoints=1, labelspacing=0.1, handletextpad=0.4,
+ax[1][0].legend( loc='upper left', numpoints=1, labelspacing=0.1, handletextpad=0.4,
               borderpad=0.4, handlelength=2.7, fontsize='large' )
 
-ax[0][1].legend( loc='lower right', numpoints=1, labelspacing=0.1, handletextpad=0.4,
+ax[0][1].legend( loc='upper left', numpoints=1, labelspacing=0.1, handletextpad=0.4,
               borderpad=0.4, handlelength=2.7, fontsize='large' )
 
 ax[1][1].legend( loc='lower left', numpoints=1, labelspacing=0.1, handletextpad=0.4,
